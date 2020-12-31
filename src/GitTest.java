679c278831601bf413fffa71985ed95964f97d84
OutsideLunch <ghrjf94@gmail.com>
2020-12-31 11:46:03 +0900

    STS 에서 수정 후 Source Tree 에서 커밋

--- src/GitTest.java
+++ src/GitTest.java
@@ -3,8 +3,9 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("GitTest");
+		System.out.println("GitTest1");
 		System.out.println("GitTest2");
+		System.out.println("GitTest3 소스트리와 연동");
 	}
 
 }
